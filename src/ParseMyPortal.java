791cc7b8e147a074df655844fed648b1c5a8e22d
weisscoding <30901564+weisscoding@users.noreply.github.com>
2017-11-18 16:44:37 -0800

    parsing myportal - robust

---
+++ src/ParseMyPortal.java
@@ -0,0 +1,15 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ParseMyPortal {
+    public static void main(String[] args) throws IOException {
+        File input = new File("resources/inputfiles/mp");
+        //Document doc = Jsoup.parse(input, "UTF-8", "http://example.com/");
+        //System.out.println("hmm");
+
+
+    }
+}
