037e2be2e80b39e0eb3cd2818332ce4ae538fcb2
weisscoding <30901564+weisscoding@users.noreply.github.com>
2017-11-22 13:43:02 -0800

    parsing complete

--- src/structures/ClassTime.java
+++ src/structures/ClassTime.java
@@ -2,22 +2,38 @@ package structures;
 
 public class ClassTime {
     private int timestamp;
-    private String instructor;
+    private String[] instructors;
     private String location;
     private String attribute;
 
     public ClassTime(int timestamp,  String instructor, String location, String attribute){
         this.timestamp = timestamp;
-        this.instructor = instructor;
+
+        setInstructors(instructor);
         this.location = location;
         this.attribute = attribute;
     }
 
     public String toJSON(){
-        return "{\"timestamp\":"+timestamp+"," +
-                "\"instructor\":\""+instructor+"\"," +
-                "\"location\":\""+location+"\"," +
+
+        String JSON = "{\"timestamp\":"+timestamp + ",\"instructors\":[";
+
+        for(String instructor : instructors){
+            JSON += "\""+instructor+"\",";
+        }
+
+        JSON = JSON.substring(0,JSON.length() - 1) + "],"
+                +"\"location\":\""+location+"\"," +
                 "\"attribute\":\""+attribute+"\"}";
+
+        return JSON;
     }
 
+    private void setInstructors(String instructorString) {
+        String[] instructors = instructorString.split(",");
+        for(int i = 0; i < instructors.length; i++){
+            instructors[i] = instructors[i].trim();
+        }
+        this.instructors = instructors;
+    }
 }
